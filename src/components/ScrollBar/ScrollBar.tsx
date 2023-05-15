2ddc0f76a3c7cf955efc70c9ab4fe847c772268d
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-05-15 22:20:04 +0300

    init scrollbar

--- src/components/ScrollBar/ScrollBar.tsx
+++ src/components/ScrollBar/ScrollBar.tsx
@@ -4,7 +4,10 @@ import './ScrollBar.modul.scss'
 export const ScrollBar = () => {
   return (
     <div className={'scrollbar'}>
-        
+        <div className={'element'}>01</div>
+        <div className={'element'}>02</div>
+        <div className={'element'}>03</div>
+        <div className={'element'}>04</div>
     </div>
   )
 }

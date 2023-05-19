d402f04d67f86aa21e28f1cfd3918f94159f91e8
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-05-19 16:58:06 +0300

    add second page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Home.modul.scss'
 import { ScrollBar } from '../ScrollBar/ScrollBar'
 import { MainPage } from '../MainPage/MainPage'
+import {ReasonsToCooperate} from '../ReasonsToCooperate/ReasonsToCooperate'
 
 export const Home = () => {
   return (
     <div className={'main'}>
         <ScrollBar />
         <MainPage />
+        <ReasonsToCooperate />
     </div>
   )
 }
